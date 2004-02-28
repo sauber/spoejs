a7cce6c938c71f676f7da53087ff2a007c07f4d1
snicki <snicki>
2004-02-28 04:57:07 +0000

    *** empty log message ***

--- test/StoryList.pl
+++ test/StoryList.pl
@@ -11,6 +11,26 @@ my $dir = $SL->add_story();
 
 $SL->del_story( story => $dir );
 
-my @sorted_stories = $SL->list_stories();
+my @all_stories = $SL->list_stories();
+print "All stories:\n";
+print $_->date(), "\n" for @all_stories;
 
-print $_->date(), "\n" for @sorted_stories;
+@count_stories = $SL->list_stories( count => 3 );
+print "'count' stories:\n";
+print $_->date(), "\n" for @count_stories;
+
+my @cat_stories = $SL->list_stories( category => 'sport' );
+print "'category' stories:\n";
+print $_->date(), "\n" for @cat_stories;
+
+my @range_stories = $SL->list_stories( from=>'2004/02/04', to=>'2004/02/08' );
+print "range stories:\n";
+print $_->date(), "\n" for @range_stories;
+
+my @prev_stories = $SL->list_stories( story=>'2004/02/04', prev => 3 );
+print "prev stories:\n";
+print $_->date(), "\n" for @prev_stories;
+
+my @next_stories = $SL->list_stories( story=>'2004/02/04', next => 4 );
+print "next stories:\n";
+print $_->date(), "\n" for @next_stories;
