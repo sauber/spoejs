aa0826a20dd1498ffd2098665f3949b0e63341dc
snicki <snicki>
2004-02-29 07:25:13 +0000

    Initial add

---
+++ lib/Spoejs/Pic.pm
@@ -0,0 +1,25 @@
+package Spoejs::Pic;
+
+use base ( "Media" );
+
+# $Id: Pic.pm,v 1.1 2004/02/29 07:25:13 snicki Exp $
+$Spoejs::Pic::VERSION = $Spoejs::Pic::VERSION = '$Revision: 1.1 $';
+
+#### Private helper functions ####
+
+
+#### Public interface ####
+
+# Get 'blob' and store as 'filename'
+#
+sub save {
+
+
+}
+
+# Load file using ImageMagick and return ref to IM objekt
+#
+sub load {
+
+
+}
