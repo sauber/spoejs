118a9928655eef835884d046ebc734c848bb3389
snicki <snicki>
2004-03-01 10:48:48 +0000

    added lang_list()

--- lib/Spoejs/Lang.pm
+++ lib/Spoejs/Lang.pm
@@ -1,9 +1,11 @@
 package Spoejs::Lang;
 use base ( "Spoejs" );
+use utf8;
+use Locale::Language;
+use Data::Dumper;
 
-# $Id: Lang.pm,v 1.1 2004/02/27 05:10:53 snicki Exp $
-$Spoejs::Lang::VERSION = $Spoejs::Lang::VERSION = '$Revision: 1.1 $';
-
+# $Id: Lang.pm,v 1.2 2004/03/01 10:48:48 snicki Exp $
+$Spoejs::Lang::VERSION = $Spoejs::Lang::VERSION = '$Revision: 1.2 $';
 
 sub _initialize {
     my $self = shift;
@@ -53,4 +55,18 @@ sub tr {
     }
     
     return %{$data};
+}
+
+# Todo:
+# 1. Get resource.txt location automatically
+# 2. Get language list automatically
+# 
+sub lang_list {
+    my $self = shift;
+    my @twoletter = qw( en dk de fr se jp zh gr ar it );
+    my $T = new Spoejs::Text(  lang=>$self, 
+	            full_path => '/usr/local/wwwdoc/spoejs/lib/resource.txt' );
+    my %langs = $T->get( @twoletter );
+
+    return %langs;
 }
