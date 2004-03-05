7e5859fa147bcc089d14acd21b3694f04530daee
sauber <sauber>
2004-03-05 03:57:29 +0000

    Sub routine descriptions.

--- lib/Spoejs/Resource.pm
+++ lib/Spoejs/Resource.pm
@@ -1,9 +1,11 @@
 package Spoejs::Resource;
 use base ( "Spoejs::Text" );
 
-# $Id: Resource.pm,v 1.5 2004/03/04 11:43:52 snicki Exp $
-$Spoejs::Resource::VERSION = $Spoejs::Resource::VERSION = '$Revision: 1.5 $';
+# $Id: Resource.pm,v 1.6 2004/03/05 03:57:29 sauber Exp $
+$Spoejs::Resource::VERSION = $Spoejs::Resource::VERSION = '$Revision: 1.6 $';
 
+# Set path and filename for resource file
+#
 sub _initialize {
     my $self = shift;
     my $path = $ENV{DOCUMENT_ROOT} || '';
@@ -13,6 +15,7 @@ sub _initialize {
 }
 
 # Generate list of languages.
+#
 sub lang_list {
   my($self) = @_;
 
