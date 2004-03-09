9428aeb081b941f371722100f87bbe8ce7385475
snicki <snicki>
2004-03-09 01:42:13 +0000

    Rework channel_dir()

--- lib/Spoejs/ChannelConf.pm
+++ lib/Spoejs/ChannelConf.pm
@@ -1,8 +1,8 @@
 package Spoejs::ChannelConf;
 use base ( "Spoejs::Text" );
 
-# $Id: ChannelConf.pm,v 1.3 2004/03/06 09:16:24 snicki Exp $
-$Spoejs::ChannelConf::VERSION = $Spoejs::ChannelConf::VERSION = '$Revision: 1.3 $';
+# $Id: ChannelConf.pm,v 1.4 2004/03/09 01:42:13 snicki Exp $
+$Spoejs::ChannelConf::VERSION = $Spoejs::ChannelConf::VERSION = '$Revision: 1.4 $';
 
 sub _initialize {
     my $self = shift;
@@ -13,18 +13,21 @@ sub _initialize {
 # Static function - doesn't need initialized object
 # Text.pm pt. depends on that
 #
-sub channel_dir
-{
-    my $dir = $ENV{REQUEST_URI};
-    my $root = $ENV{DOCUMENT_ROOT};
+#sub channel_dir
+#{
+#    my $root = $ENV{DOCUMENT_ROOT};
 
     # For testing purposes outside apache env.
-    $root ||= "/usr/local/wwwdoc/spoejs/htdocs/";
-    $dir  ||= "/test";
+#    $root ||= "/usr/local/wwwdoc/spoejs/htdocs/";
 
-    $dir =~ s/(\/\w+\.html)$//;
 
-    return $root . "users" . $dir;
+#    return $root . "users" . user_dir();
+#}
+
+# 
+#
+sub channel_dir {
+    my $self = shift;
+    @dirs = reverse split /\//, $self->{path}; 
+    return $dirs[0];
 }
-
-1;
