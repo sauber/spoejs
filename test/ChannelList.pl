d841558e26356a17ed0a9284e05bc18955073156
snicki <snicki>
2004-03-11 09:29:22 +0000

    add new_channel() test

--- test/ChannelList.pl
+++ test/ChannelList.pl
@@ -5,6 +5,12 @@ use Data::Dumper;
 
 my $CL = new Spoejs::ChannelList( path => '../htdocs/users/' );
 
+my $res = $CL->new_channel( username => 'snicki', password => 'kodeord',
+			    title=>'Test channel', shortname => 'testchan',
+			    description => 'This is just a test channel' );
+
+die $CL->{msg} unless $res;
+
 my @channels = $CL->search_channels( username => 'snicki',
 				     password => 'kodeord');
 
