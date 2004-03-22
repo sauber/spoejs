5ed3cd5f9c8566d5d9b3d874c1b186f8ea3ff26d
snicki <snicki>
2004-03-22 12:19:37 +0000

    Add prev/next functions

--- lib/Spoejs/MediaList.pm
+++ lib/Spoejs/MediaList.pm
@@ -1,8 +1,8 @@
 package Spoejs::MediaList;
 use base ( "Spoejs::List" );
 
-# $Id: MediaList.pm,v 1.1 2004/03/22 07:09:00 snicki Exp $
-$Spoejs::MediaList::VERSION = $Spoejs::MediaList::VERSION = '$Revision: 1.1 $';
+# $Id: MediaList.pm,v 1.2 2004/03/22 12:19:37 snicki Exp $
+$Spoejs::MediaList::VERSION = $Spoejs::MediaList::VERSION = '$Revision: 1.2 $';
 
 # Should be called with 'path' to directory containing the media
 sub _initialize {
@@ -10,15 +10,54 @@ sub _initialize {
     $self->SUPER::_initialize( @_ );
 }
 
+#### Private helper functions ####
+
+sub _next {
+    my ( $self, $current, @media ) = @_;
+    $current =~ s{(.*)(...)}{$1\.$2}g;
+    my $found;
+    for ( @media ) {
+	if ($found) { $found = $_->{path}; last; }
+	$found = 1 if ( $_->{path} =~ /$current/ );
+    }
+
+    return undef if $found == 1;
+
+    my @p = reverse split /\//, $found;
+    $found = $p[0];
+    $found =~ s{\.}{}g;
+    return $found;
+}
+
+
+
+
+
+#### Public interface ####
+
 sub list {
     my $self = shift;
 
-    my @files = $self->_list_from_file_pattern( "jpg|JPG" );
+    my @files = $self->_list_from_file_pattern( '(jpg|JPG)$' );
     my @items;
 
-    for ( @files ) {
+    for ( sort @files ) {
 	push @items, new Spoejs::Pic( path => $_ );
     }
 
     return @items;
+}
+
+
+sub next {
+    my ( $self, $current ) = @_;
+
+    return $self->_next( $current, $self->list() );
+}
+
+
+sub prev {
+    my ( $self, $current ) = @_;
+
+    return $self->_next( $current, reverse $self->list() );
 }
