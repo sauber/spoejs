8c327eadc621f268c9e41e96506177afc76bd23e
snicki <snicki>
2004-03-29 11:39:38 +0000

    mostly cleanup

--- lib/Spoejs/Media.pm
+++ lib/Spoejs/Media.pm
@@ -11,8 +11,8 @@ use Bootstring;
 no Carp::Assert;
 use base ( "Spoejs" );
 
-# $Id: Media.pm,v 1.8 2004/03/29 08:22:11 snicki Exp $
-$Spoejs::Media::VERSION = $Spoejs::Media::VERSION = '$Revision: 1.8 $';
+# $Id: Media.pm,v 1.9 2004/03/29 11:39:38 snicki Exp $
+$Spoejs::Media::VERSION = $Spoejs::Media::VERSION = '$Revision: 1.9 $';
 
 #### Private helper functions ####
 
@@ -20,20 +20,20 @@ $Spoejs::Media::VERSION = $Spoejs::Media::VERSION = '$Revision: 1.8 $';
 #
 sub valid_name {
     my($self) = @_;
-
+    
     my ($file, $ext);
     $file = $self->{file};
-    $file =~ s/(.*)(\.)(....?)$/$1/ and $ext = $3;
+    $file =~ s/(.*)(\.)(....?)$/$1/ and $ext = lc $3;
     my @basic = eval "a..z, A..Z, 0..9";
-  my $BS = new Bootstring( BASIC => \@basic, 
-	TMAX => 53,
-	SKEW => 78,
-	INITIAL_BIAS => 32,
-	TMIN => 38,
-	DAMP => 40,
- );
+    my $BS = new Bootstring( BASIC => \@basic,
+			     TMAX => 53,
+			     SKEW => 78,
+			     INITIAL_BIAS => 32,
+			     TMIN => 38,
+			     DAMP => 40,
+			     );
     $file = $BS->encode( $file );
-
+    
     $self->{file} = "$file.$ext";
 };
 
@@ -53,7 +53,7 @@ sub scale {
   }
 
   my($w,$h) = $self->info();
-  warn "Got zero width or height: w: $w, h: $h";
+  warn "Got zero width or height: w: $w, h: $h" if $w == 0 or $h == 0;
   return $self->_err( "Got zero width or height" ) if $w == 0 or $h == 0;
   if ( $w>$h ) { $x=$m; $y = int .5 + $m*$h/$w }
           else { $y=$m; $x = int .5 + $m*$w/$h }
@@ -101,15 +101,16 @@ sub utf_filename {
 
 sub rename {
     my ( $self, $new_file ) = @_;
-
+    
     my $old_file = "$self->{path}/$self->{file}";
-
+    
     my $BS = _create_bs();
-
+    
     $self->{file} = $new_file;
     $self->valid_name();
 
-    rename "$old_file", "$self->{path}/$self->{file}" or warn "rename: $!";
+    rename "$old_file", "$self->{path}/$self->{file}" or 
+	return $self->_err( "rename failed: $!" );
 }
 
 
@@ -121,11 +122,11 @@ sub delete {
 # Get info (IM->Ping) about pic
 #
 sub ping {
-  my( $self, %params ) = @_;
-
-  return undef unless ( $self->{file} && $self->{path} );
-
-  # Use Image:Magick's ping to get resolution of image
-  my $im = Image::Magick->new();
-  return $im->Ping( "$self->{path}/$self->{file}" );
+    my( $self, %params ) = @_;
+    
+    return undef unless ( $self->{file} && $self->{path} );
+    
+    # Use Image:Magick's ping to get resolution of image
+    my $im = Image::Magick->new();
+    return $im->Ping( "$self->{path}/$self->{file}" );
 }
