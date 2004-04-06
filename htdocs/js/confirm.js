7246701b2d6e5c0b91f5fd304b93dd70e682ea3a
snicki <snicki>
2004-04-06 06:44:18 +0000

    simple confirm dialog

---
+++ htdocs/js/confirm.js
@@ -0,0 +1,10 @@
+<script language="javascript" type="text/javascript">
+function confirmSubmit()
+{
+var agree=confirm("Are you sure you wish to continue?");
+if (agree)
+  return true ;
+else
+  return false ;
+}
+</script>
