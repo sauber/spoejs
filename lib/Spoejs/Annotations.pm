273e95971fe4d574a71b863ed7e0c96b953522f2
snicki <snicki>
2004-05-20 10:51:50 +0000

    remove warn

--- lib/Spoejs/Annotations.pm
+++ lib/Spoejs/Annotations.pm
@@ -1,15 +1,14 @@
 package Spoejs::Annotations;
 use base ( "Spoejs::Text" );
 
-# $Id: Annotations.pm,v 1.1 2004/05/20 10:11:06 snicki Exp $
-$Spoejs::Annotations::VERSION = $Spoejs::Annotations::VERSION = '$Revision: 1.1 $';
+# $Id: Annotations.pm,v 1.2 2004/05/20 10:51:50 snicki Exp $
+$Spoejs::Annotations::VERSION = $Spoejs::Annotations::VERSION = '$Revision: 1.2 $';
 
 sub _initialize {
     my $self = shift;
     $self->SUPER::_initialize( @_, file => 'annotations.txt' );
     return $self->_err( "Missing or invalid path: $self->{path}" )
         unless $self->{path};
-    warn "path: $self->{path}  :  file: $self->{file}";
 }
 
 __END__
