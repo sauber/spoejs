320a3f14d7d60a99265515f1bb8c56676b27b84e
sauber <sauber>
2004-08-13 09:45:25 +0000

    Old perl hack

--- test/imageinfo.pl
+++ test/imageinfo.pl
@@ -1,9 +1,17 @@
 #!/usr/bin/perl
 
-# Image::Info sometimes hang :-(
 my %picinfo;
+# Signals does not work correct in modperl comtext in perl <5.8
+# Image::Info sometimes hang :-(
+
 eval {
-  local $SIG{ALRM} = sub { die "timeout\n" };
+  if ( $[ < 5.008 ) {
+    # Modperl, perl 5.6.1 and signals require special attention
+    use Sys::Signal ();
+    my $h = Sys::Signal->set(ALRM => sub { die "timeout\n" });
+  } else {
+    local $SIG{ALRM} = sub { die "timeout\n" };
+  }
   alarm 2;
   use Image::Info qw(image_info);
   my $info = image_info($ARGV[0]);
@@ -16,8 +24,8 @@ eval {
   alarm 0;
 };
 if ($@) {
-     die unless $@ eq "timeout\n";   # propagate unexpected errors
-     # timed out
+  die unless $@ eq "timeout\n";   # propagate unexpected errors
+  # timed out
 }
 
 for ( sort { lc($a) cmp lc($b)} keys %picinfo ) {
