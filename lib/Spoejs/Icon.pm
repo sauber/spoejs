1b7dfcf6327b8e705de31c0df8e31d712581c7e9
sauber <sauber>
2004-08-16 11:02:05 +0000

    Support for local icon files

--- lib/Spoejs/Icon.pm
+++ lib/Spoejs/Icon.pm
@@ -2,27 +2,30 @@ package Spoejs::Icon;
 use LWP::UserAgent;
 use base ( "Spoejs::Media" );
 
-# $Id: Icon.pm,v 1.12 2004/05/08 14:51:01 sauber Exp $
-$Spoejs::Icon::VERSION = $Spoejs::Icon::VERSION = '$Revision: 1.12 $';
+# $Id: Icon.pm,v 1.13 2004/08/16 11:02:05 sauber Exp $
+$Spoejs::Icon::VERSION = $Spoejs::Icon::VERSION = '$Revision: 1.13 $';
+
+# Initializor
+#
+sub _initialize {
+  my($self) = shift;
+
+  # Default path for site local icon library
+  $self->{path} ||= "$ENV{DOCUMENT_ROOT}/../lib/icons";
+}
 
 #### Private interface ####
 
 # Search on google and download the most square icon
 #
 sub _downloadicon {
-  my($self,$category,$size,$sizeset) = @_;
+  my($self,$category,$size) = @_;
 
-  # Google only accept well know agents
   my $ua = LWP::UserAgent->new;
   $ua->agent('Lynx/2.8.4rel.1 libwww-FM/2.14');
 
   # Perform the search
-  $category =~ s/([\W])/"%".uc(sprintf("%2.2x",ord($1)))/eg;
-  $category =~ s/%20/+/g;
-  my $url = 'http://images.google.com/images?ie=UTF-8&oe=UTF-8&q='
-          . $category;
-  $url .= "&imgsz=$sizeset" if $sizeset;
-  my $r= $ua->get($url);
+  my $r= $ua->get("http://images.google.com/images?q=%22$category%22&imgsz=small");
   my ($index);
   if ($r->is_success) {
     $index = $r->content;
@@ -44,9 +47,9 @@ sub _downloadicon {
   for $n ( 0 .. $#p ) {
     my($x,$y) = $p[$n] =~ m/\d+/g;
     my $s = $y/$x; $s = 1/$s if $s > 1;
-    # Also consider size of picture if desired size is specified
+    # Also consider size of picture if size desired size is specified
     if ( $size ) {
-      my $xy = $size / sqrt( $x**2 + $y**2 ); $xy = 1/$xy if $xy > 1;
+      my $xy = ( $x * $y ) / ( $size * $size ); $xy = 1/$xy if $xy > 1;
       $s *= $xy;
     }
     $I[$n]{link} = $l[$n];
@@ -54,10 +57,10 @@ sub _downloadicon {
   }
 
   # Did we get at least one picture? Otherwise pass on Google's
-  # misspelling suggestion or any icon.
+  # misspelling suggestion or unknown.
   unless ( @I ) {
     $index =~ m/Did you mean:.*?<i>(.*?)<\/i>/;
-    $self->{_didyoumean} = $1 || 'icon';
+    $self->{_didyoumean} = $1 || 'unknown';
     return $self->_err('No mathing pictures. Try alternative.');
   }
 
@@ -75,19 +78,62 @@ sub _downloadicon {
   }
 };
 
+# Local icon from disk
+#
+sub _localloadicon {
+  my($self,$category) = @_;
+
+  $self->_create_bs();
+  my $bscat = $self->{_BS}->encode($category);
+  #warn "Cat=$category bscat=$bscat";
+
+  for my $f ( $self->_list_local() ) {
+    #warn "Testing local file $f";
+    if ( $f =~ /^$bscat\.\w+$/ ) {
+      # Local icon that matches category is found
+      $self->{file} = $f;
+      #warn "$f matches $bscat";
+      return $self->load();
+    }
+  }
+
+  #No matching local icon file
+  return undef;
+}
+
+# Read list of local icon files from disk
+#
+sub _list_local {
+  my($self) = @_;
+
+  # Check that path is specified and dir exists
+  return $self->_err("Path missing") unless $self->{path};
+  return $self->_err("Path not readable") unless -d $self->{path};
+
+  # Read files that has underscore and extension
+  opendir DH, $self->{path};
+    my @files = grep /_.*\.\w+/, readdir DH;
+  closedir DH;
+
+  return @files;
+}
+
+
 #### Public interface ####
 
 sub get {
   my($self,%data) = @_;
 
-  # Try several times to get an icon
-  $self->_downloadicon($data{category},$data{size},'small')
+  # Try several times to get an icon, first local, then from google
+  $self->_localloadicon($data{category})
+    || $self->_downloadicon($data{category},$data{size},'small')
     || $self->_downloadicon($self->{_didyoumean},$data{size},'small')
     || $self->_downloadicon($data{category},$data{size})
     || $self->_downloadicon($self->{_didyoumean},$data{size});
   return $self->_err('No picture downloaded') unless $self->{_blob};
 
-  # If size is specified then scale
+  # If size is specified then convert to imagemagick object, scale, and
+  # convert back to jpeg again.
   if ( $data{size} ) {
     $self->scale($data{size});
   }
@@ -95,15 +141,18 @@ sub get {
   return \$self->{_blob};
 }
 
-__END__
-                                                                                
-=head1 NAME
-                                                                                
-Spoejs::Icon - Generate icon images using Google image search
+# Generate list of static categories on server
+#
+sub list {
+  my($self) = @_;
 
-=head1 LICENSE 
+  # Read list of files
+  my @files = $self->_list_local();
+  return undef if @files and not $files[0];
 
-Artistic License
-http://www.opensource.org/licenses/artistic-license.php
-                                                                                
-=cut
+  # Return list of BS decoded filenames without extensions.
+  $self->_create_bs();
+  return map { /(.*)\.\w+/ and $self->{_BS}->decode($1) } @files;
+}
+
+1;
